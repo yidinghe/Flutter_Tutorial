0c3051f1453bc770b7bdb14ec4bed9366e0d87a0
yidinghe <yidinghe2012@gmail.com>
2018-03-30 16:26:33 -0400

    Finished next rows and columns page.
    TODO, fix the overflowed UI bug.

--- lib/BuildLayoutsInFlutterPages/3_nest_rows_columns.dart
+++ lib/BuildLayoutsInFlutterPages/3_nest_rows_columns.dart
@@ -33,6 +33,126 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var titleText = new Container(
+      padding: new EdgeInsets.all(20.0),
+      child: new Text(
+        'Strawberry Pavlova',
+        style: new TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+        ),
+      ),
+    );
+
+    var subTitle = new Text(
+      '''
+Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.
+''',
+      textAlign: TextAlign.center,
+      style: new TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25.0,
+      ),
+    );
+
+    var ratings = new Container(
+      padding: new EdgeInsets.all(20.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+            ],
+          ),
+          new Text(
+            '170 Reviews',
+            style: new TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    var descTextStyle = new TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18.0,
+      height: 2.0,
+    );
+
+    var iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: new Container(
+        padding: new EdgeInsets.all(20.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new Icon(
+                  Icons.kitchen,
+                  color: Colors.green[500],
+                ),
+                new Text('PREP:'),
+                new Text('25 min'),
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                new Icon(
+                  Icons.timer,
+                  color: Colors.green[500],
+                ),
+                new Text('COOK:'),
+                new Text('1 hr'),
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                new Icon(
+                  Icons.restaurant,
+                  color: Colors.green[500],
+                ),
+                new Text('FEEDS:'),
+                new Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    var leftColumn = new Container(
+      padding: new EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+      child: new Column(
+        children: <Widget>[
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    var mainImage = new Image.asset(
+      'images/berries.jpg',
+      fit: BoxFit.cover,
+    );
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(
@@ -49,13 +169,9 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 new Container(
                   width: 440.0,
-                  child: new Image.asset(
-                    'images/lake.jpg',
-                    width: 300.0,
-                    height: 240.0,
-                    fit: BoxFit.cover,
-                  ),
-                )
+                  child: leftColumn,
+                ),
+                mainImage,
               ],
             ),
           ),
