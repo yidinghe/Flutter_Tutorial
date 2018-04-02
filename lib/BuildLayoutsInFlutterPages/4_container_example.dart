4f54091713d19b53b55e9894e7bff8eb2055e50c
yidinghe <yidinghe2012@gmail.com>
2018-04-02 17:03:16 -0400

    Added container_example.

---
+++ lib/BuildLayoutsInFlutterPages/4_container_example.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var boxDecoration = new BoxDecoration(
+      border: new Border.all(width: 10.0, color: Colors.black38),
+      borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
+    );
+
+    var container = new Container(
+      decoration: new BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: new Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new Container(
+                  decoration: boxDecoration,
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/lake.jpg'),
+                ),
+              ),
+              new Expanded(
+                child: new Container(
+                  decoration: boxDecoration,
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/lake.jpg'),
+                ),
+              ),
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new Container(
+                  decoration: boxDecoration,
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/berries.jpg'),
+                ),
+              ),
+              new Expanded(
+                child: new Container(
+                  decoration: boxDecoration,
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/berries.jpg'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(child: container),
+    );
+  }
+}
