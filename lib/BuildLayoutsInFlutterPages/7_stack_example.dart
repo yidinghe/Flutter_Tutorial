8768c8477840c486bd6a9195e05a2b9915a9f954
yidinghe <yidinghe2012@gmail.com>
2018-04-04 15:04:14 -0400

    Added stack example.

---
+++ lib/BuildLayoutsInFlutterPages/7_stack_example.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Stack Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var stack = new Stack(
+      //alignment from -1.0 to 1.0
+      alignment: const Alignment(0.6, 0.6),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new AssetImage('images/pic1.jpg'),
+          radius: 100.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: new Text(
+            'Mia B',
+            style: new TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+        elevation: 5.0,
+      ),
+      body: new Center(
+        child: stack,
+      ),
+    );
+  }
+}
