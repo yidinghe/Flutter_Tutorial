cc80aa757b4560b7d825cfab587e54f3d080df46
yidinghe <yidinghe2012@gmail.com>
2018-04-04 15:19:56 -0400

    Finished card example 1.

--- lib/BuildLayoutsInFlutterPages/8_card_example_1.dart
+++ lib/BuildLayoutsInFlutterPages/8_card_example_1.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Stack Demo Home Page'),
+      home: new MyHomePage(title: 'Card Demo Home Page'),
     );
   }
 }
@@ -27,12 +27,53 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var card = new Card(
+      elevation: 12.0,
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+            title: new Text(
+              '1625 Main Street',
+              style: new TextStyle(fontWeight: FontWeight.w500),
+            ),
+            subtitle: new Text('My City, CA 94484'),
+            leading: new Icon(
+              Icons.restaurant_menu,
+              color: Colors.blue[500],
+            ),
+          ),
+          new Divider(),
+          new ListTile(
+            title: new Text('(408) 555-1212',
+                style: new TextStyle(fontWeight: FontWeight.w500)),
+            leading: new Icon(
+              Icons.contact_phone,
+              color: Colors.blue[500],
+            ),
+          ),
+          new ListTile(
+            title: new Text('costa@example.com'),
+            leading: new Icon(
+              Icons.contact_mail,
+              color: Colors.blue[500],
+            ),
+          ),
+        ],
+      ),
+    );
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
         elevation: 5.0,
       ),
-      body: new Center(),
+      body: new Center(
+        child: new SizedBox(
+          width: 450.0,
+          height: 210.0,
+          child: card,
+        ),
+      ),
     );
   }
 }
--- lib/BuildLayoutsInFlutterPages/8_card_example_2.dart
+++ lib/BuildLayoutsInFlutterPages/8_card_example_2.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Stack Demo Home Page'),
+      home: new MyHomePage(title: 'Card Demo Home Page'),
     );
   }
 }
