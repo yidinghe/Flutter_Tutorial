1d18e4919736e3c99a3f1c5cec5ebe2c08cd099e
yidinghe <yidinghe2012@gmail.com>
2018-04-09 11:52:15 -0400

    Fixed test import. Update playground to button tutorial.

--- lib/playground.dart
+++ lib/playground.dart
@@ -1,113 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
-void main() => runApp(new MyApp());
-class ParentWidget extends StatefulWidget {
-  @override
-  _ParentWidgetState createState() => new _ParentWidgetState();
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter Tutorial',
+    home: new TutorialHome(),
+  ));
 }
 
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new TapboxC(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//----------------------------- TapboxC ------------------------------
-
-class TapboxC extends StatefulWidget {
-  TapboxC({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  _TapboxCState createState() => new _TapboxCState();
-}
-
-class _TapboxCState extends State<TapboxC> {
-  bool _highlight = false;
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    widget.onChanged(!widget.active);
-  }
-
-  Widget build(BuildContext context) {
-    // This example adds a green border on tap down.
-    // On tap up, the square changes to the opposite state.
     return new GestureDetector(
-      onTapDown: _handleTapDown, // Handle the tap events in the order that
-      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
+      onTap: () {
+        print('MyButton was tapped!');
+      },
       child: new Container(
-        child: new Center(
-          child: new Text(widget.active ? 'Active' : 'Inactive',
-              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
-        ),
-        width: 200.0,
-        height: 200.0,
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
         decoration: new BoxDecoration(
-          color:
-          widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? new Border.all(
-            color: Colors.teal[700],
-            width: 10.0,
-          )
-              : null,
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.yellow[500],
+        ),
+        child: new Center(
+          child: new Text('Engage'),
         ),
       ),
     );
   }
 }
 
-//------------------------- MyApp ----------------------------------
-
-class MyApp extends StatelessWidget {
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter Demo'),
+    // Scaffold is a layout for the major Material Components.
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
         ),
-        body: new Center(
-          child: new ParentWidget(),
-        ),
+        title: new Text('Example title'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: new Center(
+        child: new MyButton(),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
