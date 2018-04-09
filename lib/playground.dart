3163eae6c37f4a9bedf7d3c7089cd1b4695debc5
yidinghe <yidinghe2012@gmail.com>
2018-04-09 17:19:18 -0400

    Updated playground.

--- lib/playground.dart
+++ lib/playground.dart
@@ -7,26 +7,51 @@ void main() {
   ));
 }
 
-class MyButton extends StatelessWidget {
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: new Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: new BoxDecoration(
-          borderRadius: new BorderRadius.circular(5.0),
-          color: Colors.yellow[500],
-        ),
-        child: new Center(
-          child: new Text('Engage'),
-        ),
-      ),
+    return new Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      onPressed: onPressed,
+      child: new Text('Increment'),
     );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => new _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(children: <Widget>[
+      new CounterIncrementor(onPressed: _increment),
+      new CounterDisplay(count: _counter),
+    ]);
   }
 }
 
@@ -52,7 +77,7 @@ class TutorialHome extends StatelessWidget {
       ),
       // body is the majority of the screen.
       body: new Center(
-        child: new MyButton(),
+        child: new Counter(),
       ),
       floatingActionButton: new FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
