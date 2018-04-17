3f4a5bd0d6e41c5d54c850f73c2f666c0c41280a
yidinghe <yidinghe2012@gmail.com>
2018-04-17 14:24:28 -0400

    1. Added Return data example. 2. Modified .gitignore.

--- lib/CookBookPages/DesignBasics/5_Add_Drawer_Example.dart
+++ lib/CookBookPages/DesignBasics/5_Add_Drawer_Example.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
+const double _kWidth = 150.0;
+
+class CustomDrawer extends Drawer {
+  const CustomDrawer({
+    Key key,
+    Widget child,
+  }) : super(key: key, child: child);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new ConstrainedBox(
+      constraints: const BoxConstraints.expand(width: _kWidth),
+      child: new Material(
+        elevation: elevation,
+        child: child,
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   final appTitle = 'Drawer Demo';
 
@@ -30,7 +51,7 @@ class MyHomePage extends StatelessWidget {
       body: new Center(
         child: new Text('My Page!'),
       ),
-      drawer: new Drawer(
+      drawer: new CustomDrawer(
         child: new ListView(
           //remove all the paddings for the listview
           padding: EdgeInsets.zero,
