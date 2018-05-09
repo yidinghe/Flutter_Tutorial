80be5093f2ba20c824a7ee1fd895d3016de0e8ba
yidinghe <yidinghe2012@gmail.com>
2018-05-09 17:14:03 -0400

    Added step2.

--- lib/6-Others/7-Flutter-Shrine-Demo/login.dart
+++ lib/6-Others/7-Flutter-Shrine-Demo/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => new _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -11,54 +11,52 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new SafeArea(
-        child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            const SizedBox(
+            SizedBox(
               height: 80.0,
             ),
-            new Column(
+            Column(
               children: <Widget>[
-                new Image.asset('images/diamond.png'),
-                const SizedBox(
+                Image.asset('images/diamond.png'),
+                SizedBox(
                   height: 16.0,
                 ),
-                const Text('SHRINE'),
+                Text('SHRINE'),
               ],
             ),
-            const SizedBox(
+            SizedBox(
               height: 120.0,
             ),
-            new TextField(
+            TextField(
               controller: _usernameController,
-              decoration:
-                  new InputDecoration(filled: true, labelText: 'Username'),
+              decoration: InputDecoration(filled: true, labelText: 'Username'),
             ),
-            new SizedBox(
+            SizedBox(
               height: 12.0,
             ),
-            new TextField(
+            TextField(
               controller: _passwordController,
-              decoration:
-                  new InputDecoration(filled: true, labelText: 'Password'),
+              decoration: InputDecoration(filled: true, labelText: 'Password'),
               obscureText: true,
             ),
-            new ButtonBar(
+            ButtonBar(
               children: <Widget>[
-                new FlatButton(
+                FlatButton(
                   onPressed: () {
                     _usernameController.clear();
                     _passwordController.clear();
                   },
-                  child: new Text('CANCEL'),
+                  child: Text('CANCEL'),
                 ),
-                new RaisedButton(
+                RaisedButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: new Text('NEXT'),
+                  child: Text('NEXT'),
                 ),
               ],
             ),
